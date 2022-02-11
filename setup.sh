e42120cda03de0602f20c5b0cb02c56893b732be
Richard <contrerasr101@gmail.com>
2022-02-10 22:44:31 -0800

    Finished setup.sh script and added alias' to zshrc

--- setup.sh
+++ setup.sh
@@ -1,20 +1,43 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+# ========== initial setup ===========
+
 termux-setup-storage
 
 apt update
 apt upgrade -y
-apt install -y vim zsh openssh wget htop netcat-openbsd
+apt install -y vim fzf zsh python3 wget htop netcat-openbsd
 
-sshd
+python3 -m pip install --upgrade pip
 
-cp vimrc ~/.vimrc
+pip3 install youtube-dl -U
+
+cp ~/android_termux/vimrc ~/.vimrc
+
+
+# ========== symlinks to most used directories ===========
+cd ~/
+
+ln -s ~/storage/downloads ~/
+ln -s ~/../usr/bin ~/
+
+
+# ========== copy scripts from my main repo ===========
+
+git clone https://github.com/richxvx/linux_files.git ~/
+
+cp ~/linux_files/scripts/* ~/bin
+
+
+# ========== setup oh-my-zsh ===========
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp zshrc ~/.zshrc
+cp ~/android_termux/zshrc ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+
+
 
 
 exit
--- zshrc
+++ zshrc
@@ -9,8 +9,8 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 alias conf="vim ~/.zshrc"
-alias install="clear; apt install"
-alias search="clear; apt search"
+alias install="apt install -y"
+alias search="apt search"
 alias yt="youtube-dl"
 alias ytmp3="youtube-dl --extract-audio --audio-format mp3"
 
@@ -24,8 +24,7 @@ alias gr='git rm'
 alias gd='git diff'
 alias gl='git log'
 
-alias ip="echo ''; echo "$(ip a | grep /24 | awk '{print $2}')"; echo ''"
-
+alias tools='sel=$(ls ~/linux_files/scripts | fzf) && vim ~/linux_files/scripts/$sel'
 
 listen() {
 
